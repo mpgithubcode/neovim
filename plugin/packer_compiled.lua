31b30289a3b3f75c9a98bad1480450946fd8c641
mpgithubcode <github@jobs.slmail.me>
2022-09-02 22:45:38 -0400

    added colorizer

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -164,6 +164,11 @@ _G.packer_plugins = {
     path = "/home/mugen/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-colorizer.lua"] = {
+    loaded = true,
+    path = "/home/mugen/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    url = "https://github.com/norcalli/nvim-colorizer.lua"
+  },
   ["nvim-dap"] = {
     loaded = true,
     path = "/home/mugen/.local/share/nvim/site/pack/packer/start/nvim-dap",
